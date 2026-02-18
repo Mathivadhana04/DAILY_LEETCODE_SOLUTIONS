7423c949b68f3e1e9fe4a891e9ee6c3ff4cd31b5
Mathivadhana <mathivsb0412@gmail.com>
2026-02-18 23:07:52 +0530

    Create 976.java
    976. Largest Perimeter Triangle

---
+++ SOLUTIONS/976.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int largestPerimeter(int[] a) {
+        Arrays.sort(a);
+        for(int i=a.length-1;i>=2;i--){
+            if(a[i]<a[i-1]+a[i-2]) return a[i]+a[i-1]+a[i-2];
+        }
+        return 0;
+    }
+}
